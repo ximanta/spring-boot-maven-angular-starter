dbb6a20bcd74f86bd04fca6498300a28de409833
Michael Hoennig <michael@hoennig.de>
2018-03-18 19:46:05 +0100

    Demo of using an API (/api/ping)
    An Angular component calls a REST API which is implemented in the
    SpringBoot App (/api/ping).
    
    Also includes jasmine test for the component.

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,27 +1,77 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
+import {TestBed, ComponentFixture, async, fakeAsync} from '@angular/core/testing';
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {AppComponent} from './app.component';
+
 describe('AppComponent', () => {
+
+  let httpMock: HttpTestingController;
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
+  const FAKE_HTTP_RESPONSE = { ping: 'pong' };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
       declarations: [
         AppComponent
       ],
-    }).compileComponents();
+    });
   }));
+
+  beforeEach(() => {
+    httpMock = TestBed.get(HttpTestingController);
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // No outstanding requests?
+
+    fixture.destroy();
+    component = null;
+  });
+
+  // ------------------------------------------------
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
+
+    waitForMockHttpResponse();
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
+
+  it('should have initial title', async(() => {
+    expect(component.title).toEqual('Angular via SpringBoot');
+
+    waitForMockHttpResponse();
   }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
+
+  it('should render title from Angular component in a <h1> tag', async(() => {
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to Angular via SpringBoot!');
+
+    waitForMockHttpResponse();
   }));
+
+  it('should render SpringBoot /api/ping response in a <p> tag', fakeAsync(() => {
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    waitForMockHttpResponse();
+    expect(compiled.querySelector('p').textContent).toContain('pong');
+  }));
+
+  // ------------------------------------------------
+
+  function waitForMockHttpResponse() {
+    const req = httpMock.expectOne('/api/ping');
+    expect(req.request.method).toBe("GET");
+    req.flush(FAKE_HTTP_RESPONSE);
+    fixture.detectChanges();
+  }
 });
--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/toPromise';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Angular via SpringBoot';
+  response = '[No response from SpringBoot backend yet.]';
+
+  constructor(private httpClient: HttpClient) {
+    this.ident()
+    .subscribe(
+      data => {
+        this.response = data['ping'];
+      },
+      error => {
+        this.response = error.message;
+    });
+  }
+
+  // just for this minimalistic example, such code would normally be in a service
+  ident(): Observable<any> {
+    return this.httpClient.get('/api/ping', {responseType: 'json'});
+  }
 }
